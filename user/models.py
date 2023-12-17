6b219ed6c18e383694d67aea6367faf3259a8b11
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-12-18 00:03:27 +0700

    fix bug

--- user/models.py
+++ user/models.py
@@ -17,6 +17,24 @@ PENDDING = "PENDDING"
 DONE = "DONE"
 
 
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active=True.")
+        user = self.create(**extra_fields)
+        user.set_password(extra_fields["password"])
+        user.save()
+        return user
+
+
 class User(AbstractUser):
     ROLE_CHOICE = [
         (MEMBER, "MEMBER"),
@@ -45,15 +63,18 @@ class User(AbstractUser):
     wallet_private_key = models.CharField(max_length=100, null=True, default=None)
     geographical_address = models.CharField(max_length=100, null=True, default="")
     introduce = models.TextField(default=None, null=True)
-    role = models.CharField(choices=ROLE_CHOICE, default=MEMBER)
+    role = models.CharField(choices=ROLE_CHOICE, default=MEMBER, max_length=255)
     otp = models.CharField(max_length=6, null=True, blank=True)
     link = models.JSONField(default=dict)
     account_balance = models.BigIntegerField(default=0)
-    confirm_status = models.CharField(choices=CONFIRM_CHOICE, default=NONE)
+    confirm_status = models.CharField(
+        choices=CONFIRM_CHOICE, default=NONE, max_length=255
+    )
     survey = models.JSONField(default=dict)
     is_active = models.BooleanField(default=False)
     is_delete = models.BooleanField(default=False)
     tx_hash = models.CharField(max_length=100, null=True, default=None)
+    objects = CustomUserManager()
 
     def __str__(self) -> str:
         return self.username
