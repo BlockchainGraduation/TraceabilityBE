5d7b1574f06d44b0ba442b3c2072ed8357abe2b9
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-12-20 22:54:16 +0700

    upadate

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --email duongtrungqb12@gmail.com --password trung2001
+python create_superuser_script.py
---
+++ create_superuser_script.py
@@ -0,0 +1,11 @@
+from .user.models import User
+
+username = "admin"
+password = "trung@gmail.com"
+
+# Kiểm tra xem superuser đã tồn tại chưa
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, "admin@example.com", password)
+    print(f"Superuser '{username}' đã được tạo thành công.")
+else:
+    print(f"Superuser '{username}' đã tồn tại.")
