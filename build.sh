bf6f45928888ba88025a307dedfb9711761258e4
DuongTrung2212 <duongtrungqb12@gmail.com>
2023-12-20 22:45:21 +0700

    update cmd

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --username admin --email duongtrungqb12@gmail.com --password trung2001
